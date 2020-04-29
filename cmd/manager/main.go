56ff4e9027a0d58c8a953c9c9457137785817d59
Sam Nguyen <khnguyen@redhat.com>
2020-04-29 15:01:20 -0400

    wip

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -92,7 +92,6 @@ func main() {
 	// Create a new Cmd to provide shared dependencies and start components
 	mgr, err := manager.New(cfg, manager.Options{
 		Namespace: namespace,
-		//MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
 	})
 	if err != nil {
 		log.Error(err, "")
