8496429f662d2f4a48f4ecf584d3a0199387659d
Alice Hubenko <ahubenko@redhat.com>
2020-10-07 11:36:25 -0700

    Moved GetMasterNodeSubnets and functions it depends on.

--- pkg/cloudclient/aws/private.go
+++ pkg/cloudclient/aws/private.go
@@ -9,9 +9,13 @@ import (
 	"strings"
 	"time"
 
+	configv1 "github.com/openshift/api/config/v1"
 	cloudingressv1alpha1 "github.com/openshift/cloud-ingress-operator/pkg/apis/cloudingress/v1alpha1"
 	utils "github.com/openshift/cloud-ingress-operator/pkg/controller/utils"
+	machineapi "github.com/openshift/machine-api-operator/pkg/apis/machine/v1beta1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	awsproviderapi "sigs.k8s.io/cluster-api-provider-aws/pkg/apis/awsproviderconfig/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/openshift/cloud-ingress-operator/pkg/config"
@@ -93,6 +97,78 @@ func (c *Client) setDefaultAPIPrivate(ctx context.Context, kclient client.Client
 	}
 	return nil
 }
+func getInfrastructureObject(kclient client.Client) (*configv1.Infrastructure, error) {
+	infra := &configv1.Infrastructure{}
+	ns := types.NamespacedName{
+		Namespace: "",
+		Name:      "cluster",
+	}
+	err := kclient.Get(context.TODO(), ns, infra)
+	if err != nil {
+		return nil, err
+	}
+	return infra, nil
+}
+
+const masterMachineLabel string = "machine.openshift.io/cluster-api-machine-role"
+
+// GetMasterNodes returns a machineList object whose .Items can be iterated
+// over to perform actions on/with information from each master machine object
+func GetMasterMachines(kclient client.Client) (*machineapi.MachineList, error) {
+	machineList := &machineapi.MachineList{}
+	listOptions := []client.ListOption{
+		client.InNamespace("openshift-machine-api"),
+		client.MatchingLabels{masterMachineLabel: "master"},
+	}
+	err := kclient.List(context.TODO(), machineList, listOptions...)
+	if err != nil {
+		return nil, err
+	}
+	return machineList, nil
+}
+
+// GetMasterNodeSubnets returns all the subnets for Machines with 'master' label.
+// return structure:
+// {
+//   public => subnetname,
+//   private => subnetname,
+// }
+//
+func GetMasterNodeSubnets(kclient client.Client) (map[string]string, error) {
+	subnets := make(map[string]string)
+	machineList, err := GetMasterMachines(kclient)
+	if err != nil {
+		return subnets, err
+	}
+	if len(machineList.Items) == 0 {
+		return subnets, fmt.Errorf("Did not find any master Machine objects")
+	}
+
+	// get the AZ from a Master object's providerSpec.
+	codec, err := awsproviderapi.NewCodec()
+
+	if err != nil {
+		return subnets, err
+	}
+
+	// Obtain the availability zone
+	awsconfig := &awsproviderapi.AWSMachineProviderConfig{}
+	err = codec.DecodeProviderSpec(&machineList.Items[0].Spec.ProviderSpec, awsconfig)
+	if err != nil {
+		return subnets, err
+	}
+
+	// Infra object gives us the Infrastructure name, which is the combination of
+	// cluster name and identifier.
+	infra, err := getInfrastructureObject(kclient)
+	if err != nil {
+		return subnets, err
+	}
+	subnets["public"] = fmt.Sprintf("%s-public-%s", infra.Status.InfrastructureName, awsconfig.Placement.AvailabilityZone)
+	subnets["private"] = fmt.Sprintf("%s-private-%s", infra.Status.InfrastructureName, awsconfig.Placement.AvailabilityZone)
+
+	return subnets, nil
+}
 
 // setDefaultAPIPublic sets the default API (api.<cluster-domain>) to public
 // scope
@@ -114,7 +190,7 @@ func (c *Client) setDefaultAPIPublic(ctx context.Context, kclient client.Client,
 		return err
 	}
 	extNLBName := infrastructureName + "-ext"
-	subnets, err := utils.GetMasterNodeSubnets(kclient)
+	subnets, err := GetMasterNodeSubnets(kclient)
 	if err != nil {
 		return err
 	}
