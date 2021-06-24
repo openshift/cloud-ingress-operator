39be96f941e2f2d21a973b23a8bd7006e34f63f2
Max Whittingham <max.whittingham@gmail.com>
2021-06-24 11:47:11 -0600

    Adding a new metric to monitor and alert if the apischeme status is not ready

--- pkg/localmetrics/localmetrics.go
+++ pkg/localmetrics/localmetrics.go
@@ -9,8 +9,13 @@ var (
 		Name: "cloud_ingress_operator_default_ingress",
 		Help: "Report if default ingress is on cluster",
 	})
+	MetricAPISchemeConditionStatus = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "cloud_ingress_operator_apischeme_status",
+		Help: "Report the status of the APIScheme status",
+	})
 
 	MetricsList = []prometheus.Collector{
 		MetricDefaultIngressController,
+		MetricAPISchemeConditionStatus,
 	}
 )
