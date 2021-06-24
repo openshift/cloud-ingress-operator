0f07202eedb5804ea072da776ab936d5f39b1d96
Max Whittingham <max.whittingham@gmail.com>
2021-06-24 11:47:11 -0600

    Initial effort
    Updated a reference in README
    
    Added vendor dir
    
    Removed vendor dir

--- pkg/localmetrics/localmetrics.go
+++ pkg/localmetrics/localmetrics.go
@@ -9,8 +9,13 @@ var (
 		Name: "cloud_ingress_operator_default_ingress",
 		Help: "Report if default ingress is on cluster",
 	})
+	MetricAPISchemeConditionStatus = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "cloud_ingress_operator_apischeme_condition",
+		Help: "Report the status of the APIScheme condition",
+	}, []string{"status", "message", "reason"})
 
 	MetricsList = []prometheus.Collector{
 		MetricDefaultIngressController,
+		MetricAPISchemeConditionStatus,
 	}
 )
