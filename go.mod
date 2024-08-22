d7e41f6f734ddccdb13e5a7f137a157e24cddde5
Ritu Mundhe <rmundhe@redhat.com>
2024-08-22 14:32:06 -0500

    sdcicd-885 migrating e2e tests (#339)
    * sdcicd-855 migrate e2e tests from osde2e
    
    * container-validate pr check fix

--- go.mod
+++ go.mod
@@ -8,16 +8,16 @@ require (
 	github.com/aws/aws-sdk-go v1.44.86
 	github.com/go-logr/logr v1.4.2
 	github.com/golang/mock v1.6.0
-	github.com/hashicorp/go-version v1.6.0
+	github.com/hashicorp/go-version v1.7.0
 	github.com/onsi/ginkgo v1.16.5
-	github.com/onsi/gomega v1.33.1
+	github.com/onsi/gomega v1.34.1
 	// go get -u github.com/openshift/api@release-4.13
 	github.com/openshift/api v0.0.0-20240522145529-93d6bda14341
 	github.com/openshift/operator-custom-metrics v0.5.1
 	github.com/operator-framework/operator-lib v0.11.0
 	github.com/prometheus/client_golang v1.19.1
 	github.com/stretchr/testify v1.9.0
-	go.uber.org/zap v1.24.0
+	go.uber.org/zap v1.25.0
 	golang.org/x/oauth2 v0.21.0
 	google.golang.org/api v0.128.0
 	gopkg.in/yaml.v2 v2.4.0
@@ -30,7 +30,8 @@ require (
 )
 
 require (
-	github.com/onsi/ginkgo/v2 v2.19.0
+	github.com/onsi/ginkgo/v2 v2.20.0
+	github.com/openshift/client-go v0.0.0-20240510131258-f646d5f29250
 	github.com/openshift/osde2e-common v0.0.0-20240705063800-9a362f696331
 )
 
@@ -40,7 +41,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
-	github.com/evanphx/json-patch v4.12.0+incompatible // indirect
+	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/go-logr/zapr v1.2.4 // indirect
@@ -54,7 +55,7 @@ require (
 	github.com/google/gnostic-models v0.6.8 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
-	github.com/google/pprof v0.0.0-20240424215950-a892ee059fd6 // indirect
+	github.com/google/pprof v0.0.0-20240727154555-813a5fbdbec8 // indirect
 	github.com/google/s2a-go v0.1.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.4 // indirect
@@ -80,16 +81,16 @@ require (
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.opencensus.io v0.24.0 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/crypto v0.24.0 // indirect
-	golang.org/x/net v0.26.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/term v0.21.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.23.0 // indirect
+	golang.org/x/term v0.23.0 // indirect
+	golang.org/x/text v0.17.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
-	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
-	gomodules.xyz/jsonpatch/v2 v2.3.0 // indirect
+	golang.org/x/tools v0.24.0 // indirect
+	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
 	google.golang.org/grpc v1.56.3 // indirect
@@ -97,8 +98,8 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/apiextensions-apiserver v0.27.2 // indirect
-	k8s.io/component-base v0.27.2 // indirect
+	k8s.io/apiextensions-apiserver v0.28.3 // indirect
+	k8s.io/component-base v0.28.3 // indirect
 	k8s.io/klog/v2 v2.120.1 // indirect
 	sigs.k8s.io/e2e-framework v0.3.0 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
--- osde2e/cloud_ingress_operator_tests.go
+++ osde2e/cloud_ingress_operator_tests.go
@@ -8,39 +8,64 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"time"
 
+	cloudingressv1alpha1 "github.com/openshift/cloud-ingress-operator/api/v1alpha1"
+
 	"github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/openshift/cloud-ingress-operator/config"
 	"github.com/openshift/osde2e-common/pkg/clients/openshift"
+
 	"golang.org/x/oauth2/google"
 	computev1 "google.golang.org/api/compute/v1"
+	"google.golang.org/api/option"
+
+	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 	logger "sigs.k8s.io/controller-runtime/pkg/log"
 
-	"github.com/aws/aws-sdk-go/aws/awserr"
-
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/elb"
-	"google.golang.org/api/option"
 )
 
 var _ = ginkgo.Describe("cloud-ingress-operator", ginkgo.Ordered, func() {
-	var k8s *openshift.Client
-	var region string
-	var provider string
-	var sts bool
+	var (
+		k8s               *openshift.Client
+		dedicatedAdmink8s *openshift.Client
+		region            string
+		provider          string
+		sts               bool
+		apiScheme         cloudingressv1alpha1.APIScheme
+		testApiScheme     *cloudingressv1alpha1.APIScheme
+	)
+	const (
+		TestPrefix             = "CloudIngressOperator"
+		defaultDesiredReplicas = 1
+		apiSchemeResourceName  = "rh-api"
+		cioServiceName         = "rh-api"
+		rhApiSvcNamespace      = "openshift-kube-apiserver"
+	)
 
 	ginkgo.BeforeAll(func(ctx context.Context) {
 		logger.SetLogger(ginkgo.GinkgoLogr)
 		var err error
 		k8s, err = openshift.New(ginkgo.GinkgoLogr)
 		Expect(err).ShouldNot(HaveOccurred(), "Unable to setup k8s client")
+		Expect(cloudingressv1alpha1.AddToScheme(k8s.GetScheme())).Should(Succeed(), "Unable to register cloudingressv1alpha1 api scheme")
+
+		dedicatedAdmink8s, err = k8s.Impersonate("test-user@redhat.com", "dedicated-admins")
+		Expect(err).ShouldNot(HaveOccurred(), "Unable to setup impersonated k8s client")
+		Expect(cloudingressv1alpha1.AddToScheme(dedicatedAdmink8s.GetScheme())).Should(Succeed(), "Unable to register cloudingressv1alpha1 api scheme")
 
 		sts, err = k8s.IsSTS(ctx)
 		Expect(err).NotTo(HaveOccurred(), "Could not determine STS config")
@@ -56,16 +81,119 @@ var _ = ginkgo.Describe("cloud-ingress-operator", ginkgo.Ordered, func() {
 		Expect(err).NotTo(HaveOccurred(), "Could not determine region")
 	})
 
+	ginkgo.It("is installed", func(ctx context.Context) {
+		ginkgo.By("Checking the deployment exists and is available")
+		var deployment appsv1.Deployment
+		err := k8s.Get(ctx, config.OperatorName, config.OperatorNamespace, &deployment)
+		Expect(err).NotTo(HaveOccurred(), "Could not get deployment")
+		readyReplicas := deployment.Status.ReadyReplicas
+		desiredReplicas := deployment.Status.Replicas
+
+		// The desired replicas should match the default installed replica count
+		Expect(desiredReplicas).To(BeNumerically("==", defaultDesiredReplicas), "The deployment desired replicas should not drift from the default")
+
+		// Desired replica count should match ready replica count
+		Expect(readyReplicas).To(BeNumerically("==", desiredReplicas), "All desired replicas should be ready.")
+	})
+
+	ginkgo.It("reconciles cidr block changes in apischeme with rh-api service", func(ctx context.Context) {
+		err := k8s.Get(ctx, apiSchemeResourceName, config.OperatorNamespace, &apiScheme)
+		Expect(err).NotTo(HaveOccurred(), "Could not get apischeme CR instance")
+		cidrBlock := apiScheme.Spec.ManagementAPIServerIngress.AllowedCIDRBlocks
+		updatedApiScheme := apiScheme.DeepCopy()
+
+		//reset cidrblock after test is done
+		defer func() {
+			updatedApiScheme.Spec.ManagementAPIServerIngress.AllowedCIDRBlocks = apiScheme.Spec.ManagementAPIServerIngress.AllowedCIDRBlocks
+			err = k8s.Update(ctx, updatedApiScheme)
+			Expect(err).NotTo(HaveOccurred(), "Could not revert APIScheme CR instance")
+		}()
+
+		// Remove last IP from the cidrBlock:
+		var updatedCidrBlock []string
+		for _, t := range cidrBlock {
+			temp := t[:]
+			updatedCidrBlock = append(updatedCidrBlock, temp)
+		}
+
+		updatedCidrBlock[len(updatedCidrBlock)-1] = ""                // Erase last element (write zero value)
+		updatedCidrBlock = updatedCidrBlock[:len(updatedCidrBlock)-1] // Truncate slice
+
+		// Put the new CIRDBlock ranges into the APIScheme
+		updatedApiScheme.Spec.ManagementAPIServerIngress.AllowedCIDRBlocks = updatedCidrBlock
+
+		// Update the APIScheme
+		err = k8s.Update(ctx, updatedApiScheme)
+		Expect(err).NotTo(HaveOccurred(), "Could not update APIScheme CR instance")
+
+		// Wait 30 secs for apiserver to reconcile
+		time.Sleep(30 * time.Second)
+
+		// Get rh-api svc
+		// Create a service Object
+		rhAPIService := new(corev1.Service)
+		err = k8s.Get(ctx, cioServiceName, rhApiSvcNamespace, rhAPIService)
+		Expect(err).NotTo(HaveOccurred(), "Could not get rh-api service")
+
+		// Make sure both the New cidrBlock and the Service LoadBalancerSourceRanges are equal
+		// If they are then the APIScheme update also updated the service.
+		Expect(updatedCidrBlock).To(BeEquivalentTo(rhAPIService.Spec.LoadBalancerSourceRanges), "Updated cidrblock from apischeme did not reflect in rh-api service")
+	})
+
+	ginkgo.It("ensures apischemes CR instance are present on cluster", func(ctx context.Context) {
+		err := wait.PollImmediate(2*time.Second, 2*time.Minute, func() (bool, error) {
+			if err := k8s.Get(ctx, apiSchemeResourceName, config.OperatorNamespace, &apiScheme); err != nil {
+				return false, nil
+			}
+			return true, nil
+		})
+		Expect(err).NotTo(HaveOccurred(), "Could not get apischeme CR instance")
+	})
+
+	ginkgo.It("ensures cluster admin can manage apischemes CR", func(ctx context.Context) {
+		defer func() {
+			k8s.Delete(ctx, testApiScheme)
+		}()
+		testApiScheme = makeApiScheme("apischeme-osde2e-test")
+		err := k8s.Create(ctx, testApiScheme)
+		Expect(err).NotTo(HaveOccurred(), "cluster admin should create apischeme CR")
+	})
+
+	ginkgo.It("ensures dedicated admin can not manage apischemes CR", func(ctx context.Context) {
+		defer func() {
+			k8s.Delete(ctx, testApiScheme)
+		}()
+		testApiScheme = makeApiScheme("apischeme-dedicated-osde2e-test")
+		err := dedicatedAdmink8s.Create(ctx, testApiScheme)
+		Expect(apierrors.IsForbidden(err)).To(BeTrue(), "no forbidden error occurred")
+		err = k8s.Get(ctx, "apischeme-dedicated-osde2e-test", config.OperatorNamespace, testApiScheme)
+		Expect(apierrors.IsNotFound(err)).To(BeTrue(), "dedicated admin should not create apischeme CR")
+	})
+
+	ginkgo.It("should resolve rh-api endpoint hostname", func(ctx context.Context) {
+		var err error
+		wait.PollUntilContextTimeout(ctx, 30*time.Second, 15*time.Minute, true, func(ctx context.Context) (bool, error) {
+			rhApiHostname, err := getLBForService(ctx, k8s, rhApiSvcNamespace, cioServiceName, true)
+			Expect(err).NotTo(HaveOccurred(), "Could not get rh-api lb")
+			_, err = net.LookupHost(rhApiHostname)
+			if err != nil {
+				return false, err
+			}
+			return true, nil
+		})
+		Expect(err).NotTo(HaveOccurred(), "Could not resolve rh-api endpoint")
+	})
+
 	if provider == "aws" {
-		ginkgo.It("manually deleted rh-api load balancer should be recreated in AWS", func(ctx context.Context) {
+		ginkgo.It("manually deleted "+cioServiceName+" load balancer should be recreated in AWS", func(ctx context.Context) {
 			awsAccessKey := os.Getenv("AWS_ACCESS_KEY_ID")
 			awsSecretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
 			Expect(awsAccessKey).NotTo(BeEmpty(), "awsAccessKey not found")
 			Expect(awsSecretKey).NotTo(BeEmpty(), "awsSecretKey not found")
 
-			ginkgo.By("Getting old rh-api load balancer name")
-			oldLBName, err := getLBForService(ctx, k8s, "openshift-kube-apiserver", "rh-api")
-			Expect(err).NotTo(HaveOccurred(), "No existing rh-api service found")
+			ginkgo.By("Getting old " + cioServiceName + " load balancer name")
+			oldLBName, err := getLBForService(ctx, k8s, rhApiSvcNamespace, cioServiceName, false)
+			Expect(err).NotTo(HaveOccurred(), "No existing "+cioServiceName+" service found")
 			log.Printf("Old load balancer name %s ", oldLBName)
 
 			// delete the load balancer in aws
@@ -85,14 +213,14 @@ var _ = ginkgo.Describe("cloud-ingress-operator", ginkgo.Ordered, func() {
 			Expect(err).NotTo(HaveOccurred(), "Could not describe old load balancer")
 			orphanSecGroupIds := oldLBDesc.LoadBalancerDescriptions[0].SecurityGroups
 
-			ginkgo.By("Deleting old rh-api load balancer")
+			ginkgo.By("Deleting old " + cioServiceName + " load balancer")
 			_, err = lb.DeleteLoadBalancer(input)
-			Expect(err).NotTo(HaveOccurred(), "Could not delete rh-api lb")
-			log.Printf("Old rh-api load balancer delete initiated")
+			Expect(err).NotTo(HaveOccurred(), "Could not delete "+cioServiceName+" lb")
+			log.Printf("Old " + cioServiceName + " load balancer delete initiated")
 
-			ginkgo.By("Waiting for rh-api service reconcile")
+			ginkgo.By("Waiting for " + cioServiceName + " service reconcile")
 			err = wait.PollUntilContextTimeout(ctx, 15*time.Second, 5*time.Minute, false, func(ctx2 context.Context) (bool, error) {
-				newLBName, err := getLBForService(ctx2, k8s, "openshift-kube-apiserver", "rh-api")
+				newLBName, err := getLBForService(ctx2, k8s, rhApiSvcNamespace, cioServiceName, false)
 				log.Printf("Looking for new load balancer")
 
 				if err != nil || newLBName == "" {
@@ -106,10 +234,10 @@ var _ = ginkgo.Describe("cloud-ingress-operator", ginkgo.Ordered, func() {
 					return true, nil
 				}
 				// the rh-api svc hasn't been deleted yet
-				log.Printf("Old rh-api service not deleted yet...")
+				log.Printf("Old " + cioServiceName + " service not deleted yet...")
 				return false, nil
 			})
-			Expect(err).NotTo(HaveOccurred(), "rh-api service did not reconcile")
+			Expect(err).NotTo(HaveOccurred(), cioServiceName+" service did not reconcile")
 
 			// old LB's security groups ("orphans") will leak if not explicitly deleted
 			// first, delete sec group rule references to the orphans
@@ -133,13 +261,13 @@ var _ = ginkgo.Describe("cloud-ingress-operator", ginkgo.Ordered, func() {
 	}
 
 	if provider == "gcp" {
-		ginkgo.It("manually deleted rh-api forwarding rule should be recreated in GCP", func(ctx context.Context) {
+		ginkgo.It("manually deleted "+cioServiceName+" forwarding rule should be recreated in GCP", func(ctx context.Context) {
 			region := os.Getenv("CLOUD_PROVIDER_REGION")
 			Expect(region).NotTo(BeEmpty(), "No CLOUD_PROVIDER_REGION set")
 
-			ginkgo.By("Getting current rh-api ip")
-			oldLBIP, err := getLBForService(ctx, k8s, "openshift-kube-apiserver", "rh-api")
-			Expect(err).NotTo(HaveOccurred(), "No existing rh-api service found")
+			ginkgo.By("Getting current " + cioServiceName + " ip")
+			oldLBIP, err := getLBForService(ctx, k8s, rhApiSvcNamespace, cioServiceName, false)
+			Expect(err).NotTo(HaveOccurred(), "No existing "+cioServiceName+" service found")
 			log.Printf("Old forwarding rule IP:  %s ", oldLBIP)
 
 			ginkgo.By("Getting GCP creds")
@@ -151,30 +279,30 @@ var _ = ginkgo.Describe("cloud-ingress-operator", ginkgo.Ordered, func() {
 			computeService, err := computev1.NewService(ctx, option.WithCredentials(gcpCreds), option.WithScopes("https://www.googleapis.com/auth/compute"))
 			Expect(err).NotTo(HaveOccurred(), "Could not initialize GCP compute service")
 
-			ginkgo.By("Getting GCP forwarding rule for rh-api")
+			ginkgo.By("Getting GCP forwarding rule for " + cioServiceName + "")
 			oldLB, err := getGCPForwardingRuleForIP(computeService, oldLBIP, project, region)
-			Expect(err).NotTo(HaveOccurred(), "Could not get forwarding rule for rh-api")
+			Expect(err).NotTo(HaveOccurred(), "Could not get forwarding rule for "+cioServiceName)
 
 			// There's no single command to delete a load balancer in GCP
 			// Deletion of any related cloud resources may delete in misconfiguration.
 			// Delete all GCP resources related to rh-api LB setup
-			ginkgo.By("Deleting GCP forwarding rule for rh-api")
+			ginkgo.By("Deleting GCP forwarding rule for " + cioServiceName)
 			if oldLB == nil {
-				log.Printf("GCP forwarding rule for rh-api does not exist; Skipping deletion ")
+				log.Printf("GCP forwarding rule for " + cioServiceName + " does not exist; Skipping deletion ")
 			} else {
 				log.Printf("Old forwarding rule name:  %s ", oldLB.Name)
 				_, err = computeService.ForwardingRules.Get(project, region, oldLB.Name).Do()
 				if err != nil {
-					log.Printf("GCP forwarding rule for rh-api not found")
+					log.Printf("GCP forwarding rule for " + cioServiceName + " not found")
 				} else {
-					ginkgo.By("Deleting GCP forwarding rule for rh-api")
+					ginkgo.By("Deleting GCP forwarding rule for " + cioServiceName)
 					_, err = computeService.ForwardingRules.Delete(project, region, oldLB.Name).Do()
 					if err != nil {
 						log.Printf("Error deleting forwarding rule")
 					}
 				}
 
-				ginkgo.By("Deleting GCP backend service rule for rh-api")
+				ginkgo.By("Deleting GCP backend service rule for " + cioServiceName)
 				_, err = computeService.BackendServices.Get(project, oldLB.Name).Do()
 				if err != nil {
 					log.Printf("GCP backend service already deleted. ")
@@ -185,7 +313,7 @@ var _ = ginkgo.Describe("cloud-ingress-operator", ginkgo.Ordered, func() {
 					}
 				}
 
-				ginkgo.By("Deleting GCP health check for rh-api ")
+				ginkgo.By("Deleting GCP health check for " + cioServiceName + " ")
 				_, err = computeService.HealthChecks.Get(project, oldLB.Name).Do()
 				if err != nil {
 					log.Printf("GCP health check already deleted ")
@@ -196,7 +324,7 @@ var _ = ginkgo.Describe("cloud-ingress-operator", ginkgo.Ordered, func() {
 					}
 				}
 
-				ginkgo.By("Deleting GCP target pool for rh-api")
+				ginkgo.By("Deleting GCP target pool for " + cioServiceName)
 				_, err = computeService.TargetPools.Get(project, region, oldLB.Name).Do()
 				if err != nil {
 					log.Printf("GCP target pool already deleted")
@@ -208,7 +336,7 @@ var _ = ginkgo.Describe("cloud-ingress-operator", ginkgo.Ordered, func() {
 				}
 			}
 
-			ginkgo.By("Deleting GCP address for rh-api")
+			ginkgo.By("Deleting GCP address for " + cioServiceName)
 			_, err = computeService.Addresses.Get(project, region, oldLBIP).Do()
 			if err != nil {
 				log.Printf("GCP IP address already deleted")
@@ -220,28 +348,28 @@ var _ = ginkgo.Describe("cloud-ingress-operator", ginkgo.Ordered, func() {
 			}
 
 			newLBIP := ""
-			ginkgo.By("Waiting for rh-api service reconcile")
+			ginkgo.By("Waiting for " + cioServiceName + " service reconcile")
 			err = wait.PollUntilContextTimeout(ctx, 15*time.Second, 10*time.Minute, true, func(ctx context.Context) (bool, error) {
 				// Getting the newly created IP from rh-api service
-				ginkgo.By("Getting new rh-api IP from rh-api service")
-				newLBIP, err = getLBForService(ctx, k8s, "openshift-kube-apiserver", "rh-api")
+				ginkgo.By("Getting new " + cioServiceName + " IP from " + cioServiceName + " service")
+				newLBIP, err = getLBForService(ctx, k8s, rhApiSvcNamespace, cioServiceName, false)
 				if (err != nil) || (newLBIP == "") {
-					log.Printf("New rh-api svc not created yet...")
+					log.Printf("New " + cioServiceName + " svc not created yet...")
 					return false, nil
 				} else if newLBIP == oldLBIP {
-					log.Printf("Old rh-api svc not deleted yet...")
+					log.Printf("Old " + cioServiceName + " svc not deleted yet...")
 					return false, nil
 				} else {
-					log.Printf("Found new rh-api svc!")
+					log.Printf("Found new " + cioServiceName + " svc!")
 					log.Printf("Reconciliation succeeded. New loadbalancer IP: %s ", newLBIP)
 					return true, nil
 				}
 			})
-			Expect(err).NotTo(HaveOccurred(), "rh-api service did not reconcile")
+			Expect(err).NotTo(HaveOccurred(), cioServiceName+" service did not reconcile")
 
-			ginkgo.By("Waiting for new rh-api forwarding rule")
+			ginkgo.By("Waiting for new " + cioServiceName + " forwarding rule")
 			err = wait.PollUntilContextTimeout(ctx, 5*time.Second, 1*time.Minute, false, func(ctx context.Context) (bool, error) {
-				ginkgo.By("Polling GCP to get new forwarding rule for rh-api")
+				ginkgo.By("Polling GCP to get new forwarding rule for " + cioServiceName)
 				newLB, err := getGCPForwardingRuleForIP(computeService, newLBIP, project, region)
 				if err != nil || newLB == nil {
 					// Either we couldn't retrieve the LB, or it wasn't created yet
@@ -258,13 +386,19 @@ var _ = ginkgo.Describe("cloud-ingress-operator", ginkgo.Ordered, func() {
 				log.Printf("Old forwarding rule not deleted yet...")
 				return false, nil
 			})
-			Expect(err).NotTo(HaveOccurred(), "New rh-api forwarding rule not created in GCP")
+			Expect(err).NotTo(HaveOccurred(), "New "+cioServiceName+" forwarding rule not created in GCP")
 		})
 	}
+
+	ginkgo.It("can be upgraded", func(ctx context.Context) {
+		ginkgo.By("forcing operator upgrade")
+		err := k8s.UpgradeOperator(ctx, config.OperatorName, config.OperatorNamespace)
+		Expect(err).NotTo(HaveOccurred(), "operator upgrade failed")
+	})
 })
 
 // getLBForService retrieves the load balancer name or IP associated with a service of type LoadBalancer
-func getLBForService(ctx context.Context, k8s *openshift.Client, namespace string, service string) (string, error) {
+func getLBForService(ctx context.Context, k8s *openshift.Client, namespace string, service string, fullHostName bool) (string, error) {
 	svc := new(corev1.Service)
 	err := k8s.Get(ctx, service, namespace, svc)
 	if err != nil {
@@ -278,6 +412,10 @@ func getLBForService(ctx context.Context, k8s *openshift.Client, namespace strin
 	if len(ingressList) == 0 {
 		// the LB wasn't created yet
 		return "", nil
+	}
+
+	if fullHostName {
+		return ingressList[0].Hostname, nil
 	}
 
 	// for GCP
@@ -370,4 +508,25 @@ func getGCPForwardingRuleForIP(computeService *computev1.Service, oldLBIP string
 	}
 
 	return oldLB, nil
+}
+
+func makeApiScheme(name string) *cloudingressv1alpha1.APIScheme {
+	apischeme := cloudingressv1alpha1.APIScheme{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "APIScheme",
+			APIVersion: cloudingressv1alpha1.GroupVersion.String(),
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: config.OperatorNamespace,
+		},
+		Spec: cloudingressv1alpha1.APISchemeSpec{
+			ManagementAPIServerIngress: cloudingressv1alpha1.ManagementAPIServerIngress{
+				Enabled:           false,
+				DNSName:           "osde2e",
+				AllowedCIDRBlocks: []string{},
+			},
+		},
+	}
+	return &apischeme
 }
