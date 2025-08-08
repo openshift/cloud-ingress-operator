578821992b37d1860e3203572b660714557f4851
sbommadi <sbommadi@redhat.com>
2025-08-08 12:26:51 -0400

    fix variable type
    Signed-off-by: sbommadi <sbommadi@redhat.com>

--- main.go
+++ main.go
@@ -210,7 +210,7 @@ func getWatchNamespaces() ([]string, error) {
 	// WatchNamespaceEnvVar is the constant for env variable WATCH_NAMESPACE
 	// which specifies the Namespace to watch.
 	// An empty value means the operator is running with cluster scope.
-	var watchNamespaceEnvVar = "WATCH_NAMESPACE"
+	const watchNamespaceEnvVar = "WATCH_NAMESPACE"
 	namespaces, found := os.LookupEnv(watchNamespaceEnvVar)
 	if !found {
 		return nil, fmt.Errorf("%s must be set", watchNamespaceEnvVar)
